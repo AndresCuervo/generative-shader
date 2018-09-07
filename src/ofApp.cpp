d29b222c6797b88a6eaf92dad6ca76d80dda14c9
Andres Cuervo <andrescuervor@gmail.com>
2018-09-07 16:53:39 -0700

    Add manual shader number injection, replace dynamically in the future!

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,103 +1,83 @@
 #include "ofApp.h"
 
+ofShader createShaderFromText(string shaderText) {
+    ofShader returnShader;
+    returnShader.setupShaderFromSource(GL_FRAGMENT_SHADER, shaderText);
+    returnShader.bindDefaults();
+    returnShader.linkProgram();
+    return returnShader;
+}
+
+string createShaderText(char *templatedShaderText, double newValue) {
+    char buffer [1000];
+    // Need to figure out how to capture values & pass them around later ...
+    // probably should use fmt: https://github.com/fmtlib/fmt
+    snprintf( buffer, 1000, templatedShaderText, newValue);
+    return buffer;
+}
+
 //--------------------------------------------------------------
 void ofApp::setup(){
-    //    shader.load("shadersGL3/shader");
-    stringstream vertexSrc;
-    vertexSrc << "#version 150\n";
-    vertexSrc << "uniform mat4 modelViewProjectionMatrix;\n";
-    vertexSrc  << "in vec4 position;\n";
-    vertexSrc  << "in vec2 texcoord;\n";
-    vertexSrc  << "out vec2 texCoordVarying;\n";
-    vertexSrc  << "void main(void){\n";
-    vertexSrc  << "\tgl_Position=modelViewProjectionMatrix*position;\n";
-    vertexSrc  << "\ttexCoordVarying = texcoord;\n";
-    vertexSrc  << "}\n";
+    // Below demonstrates the stringstream approach, but it requires converting
+    // to a string using .str() though & it's annoying to add a trailing \
+    // so I went with placing strings next to each other, for now!
+//     stringstream fragmentSrc;
+//     fragmentSrc << "\n\
+//  #ifdef GL_ES\n\
+//  precision highp float;\n\
+//  #endif\n\
+//  uniform float u_time;\
+//  uniform vec2 u_resolution;\
+//  void main() { \
+//      // Normalized pixel coordinates (from 0 to 1)\
+//      vec2 uv = gl_FragCoord.xy/u_resolution.xy;\
+//      // Time varying pixel color\
+//      vec3 col = 0.5 + %f*cos(u_time+uv.xyx+vec3(0,2,4));\
+//      gl_FragColor = vec4(col,1.0);\
+//  }";
+    fragmentSrc =
+        " #ifdef GL_ES\n"
+        " precision highp float;\n"
+        " #endif\n"
+        " uniform float u_time;"
+        " uniform vec2 u_resolution;"
+        " void main() { "
+            // Normalized pixel coordinates (from 0 to 1)
+            "vec2 uv = gl_FragCoord.xy/u_resolution.xy;"
+            // Time varying pixel color
+            "vec3 col = 0.5 + %f*cos(u_time+uv.xyx+vec3(0,2,4));"
+            // "vec3 col = 0.5 + 0.5*cos(u_time*%f+uv.xyx+vec3(0,2,4));"
+            "gl_FragColor = vec4(col,1.0);"
+        " }";
+    shader = createShaderFromText(createShaderText(fragmentSrc, defaultShaderTestValue));
 
-    // TODO: try, uhh, just instantiating this as char *text = ""\n"", where \n is an actual linebreak, see:
-    // https://stackoverflow.com/a/1135862
-    //
-    // ALsooooo, try adding holes to this & sprintf filling the holes with different values :))))
-    // ---> I guess they could also be uniforms, but that seems cumbersome, and
-    // eventually I want to be able to write these out to files & take in
-    // unmodified files, so yeah no, not uniforms for that reason, non-intrusive speculative editing!!!
-
-    stringstream fragmentSrc; fragmentSrc << "\n\ #ifdef GL_ES\n\ precision
-        highp float;\n\ #endif\n\ uniform float u_time;\n\ uniform vec2
-        u_resolution;\n\ void main() { \n\
-            // Normalized pixel coordinates (from 0 to 1)\n\
-            vec2 uv = gl_FragCoord.xy/u_resolution.xy;\n\
-            // Time varying pixel color\n\
-            vec3 col = 0.5 + 0.5*cos(u_time+uv.xyx+vec3(0,2,4));\n\
-            gl_FragColor = vec4(col,1.0);\n\ }";
-
-        /*
-#ifdef GL_ES
-precision highp float;
-#endif
-
-//--------------------------------- uniforms:
-uniform sampler2DRect fontTexture;
-uniform vec2 bounds;
-
-uniform float u_time;
-
-uniform int x_motion_on;
-uniform int x_motion_perpendicular;
-uniform float x_amplitude;
-uniform float x_speed;
-uniform float x_waviness;
-
-uniform int y_motion_on;
-uniform int y_motion_perpendicular;
-uniform float y_amplitude;
-uniform float y_speed;
-uniform float y_waviness;
-//---------------------------------
-
-void main() {
-    vec2 coord = gl_FragCoord.xy;
-    coord.y = bounds.y - coord.y; // flip along y axis for some reason.
-    if (x_motion_on != 0) {
-      float x_coord = x_motion_perpendicular != 0 ? coord.y : coord.x;
-      coord.x += x_amplitude * sin((u_time * -x_speed) + (x_coord * x_waviness));
-    }
-    if (y_motion_on != 0) {
-      float y_coord = y_motion_perpendicular != 0 ? coord.x : coord.y;
-      coord.y += y_amplitude * cos((u_time * -y_speed) + (y_coord * y_waviness));
-    }
-
-    gl_FragColor = texture2DRect(fontTexture, coord);
-}
-         * */
-
-    /*
-       void mainImage( out vec4 fragColor, in vec2 fragCoord )
-       {
-    // Normalized pixel coordinates (from 0 to 1)
-    vec2 uv = fragCoord/iResolution.xy;
-
-    // Time varying pixel color
-    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
-
-    // Output to screen
-    fragColor = vec4(col,1.0);
-    }
-    */
-
-    //    shader.setupShaderFromSource( GL_VERTEX_SHADER, vertexSrc.str() );
-    shader.setupShaderFromSource( GL_FRAGMENT_SHADER, fragmentSrc.str() );
-    shader.bindDefaults();
-    shader.linkProgram();
+    windowWidth = ofGetWidth();
+    windowHeight = ofGetHeight();
 }
 
 void ofApp::draw(){
-    ofSetColor(255);
-    shader.begin();
-    shader.setUniform1f("u_time", ofGetElapsedTimef());
-    shader.setUniform2f("u_resolution", ofGetWidth(), ofGetHeight());
-    ofDrawRectangle(0, 0, ofGetWidth(), ofGetHeight());
-    shader.end();
+    int sampleHeight = windowHeight * 0.24;
+    // ofSetColor(255);
+    // shader.begin();
+    // shader.setUniform1f("u_time", ofGetElapsedTimef());
+    // shader.setUniform2f("u_resolution", windowWidth, windowHeight);
+    // ofDrawRectangle(0, 0, windowWidth, sampleHeight);
+    // shader.end();
+
+    // Here, draw a loop going from 1 to n creating new shaders but setting the same uniforms!!!!! Just copy the current shader lines, can abstract later if you need to do this more than twice!!
+    for (int i = 1; i < 5; ++i) {
+        double changedValue = defaultShaderTestValue * (1.0/i);
+        ofLog() << i << " : " << changedValue;
+        string newShaderText = createShaderText(fragmentSrc, changedValue);
+        ofShader newShader = createShaderFromText(newShaderText);
+        ofSetColor(255);
+        newShader.begin();
+        newShader.setUniform1f("u_time", ofGetElapsedTimef());
+        newShader.setUniform2f("u_resolution", windowWidth, windowHeight);
+        ofDrawRectangle(0, sampleHeight * i - sampleHeight, windowWidth, sampleHeight);
+        newShader.end();
+    }
+    ofLog() << " ----- done ----- ";
 }
 
 //--------------------------------------------------------------
@@ -147,7 +127,8 @@ void ofApp::mouseExited(int x, int y){
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    windowWidth = ofGetWidth();
+    windowHeight = ofGetHeight();
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -4,23 +4,26 @@
 
 class ofApp : public ofBaseApp{
 
-	public:
-		void setup();
-		void update();
-		void draw();
+    public:
+        void setup();
+        void update();
+        void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-    
-    ofShader shader;
-		
+        void keyPressed(int key);
+        void keyReleased(int key);
+        void mouseMoved(int x, int y );
+        void mouseDragged(int x, int y, int button);
+        void mousePressed(int x, int y, int button);
+        void mouseReleased(int x, int y, int button);
+        void mouseEntered(int x, int y);
+        void mouseExited(int x, int y);
+        void windowResized(int w, int h);
+        void dragEvent(ofDragInfo dragInfo);
+        void gotMessage(ofMessage msg);
+
+        ofShader shader;
+        int windowHeight;
+        int windowWidth;
+        char *fragmentSrc;
+        double defaultShaderTestValue{0.5};
 };
